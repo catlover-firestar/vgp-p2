9440585100548e6632b6c0bd767c2a34c844c7c4
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2022-10-11 09:14:22 -0700

    added BonkDetector

--- My project (3)/Assets/scripts/SpawnManager.cs
+++ My project (3)/Assets/scripts/SpawnManager.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
     public GameObject[ ] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+
+    }
+    void SpawnRandomAnimal(){
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
             0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos,
             animalPrefabs[animalIndex].transform.rotation);
-        }
     }
+
 }
