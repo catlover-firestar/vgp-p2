d706c1db6ae02c56c417a3af605462fd367e433c
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2022-11-17 09:07:49 -0800

    meep meep

--- Lesson 3/Assets/Course Library/Scripts/RepeatBackground.cs
+++ Lesson 3/Assets/Course Library/Scripts/RepeatBackground.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveleft : MonoBehaviour
+public class RepeatBackground : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    private float speed = 20;
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        
     }
 }
--- Lesson 3/Assets/Course Library/Scripts/SpawnManager.cs
+++ Lesson 3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObject", startDelay, repeatRate);
     }
-
-    // Update is called once per frame
-    void Update()
+    void SpawnObject()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
+    // Update is called once per frame
 }
