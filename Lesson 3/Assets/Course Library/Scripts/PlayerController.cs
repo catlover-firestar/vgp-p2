5358ad8cdcb324a08a168894826ad35d3e86288b
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2022-11-28 09:05:46 -0800

    quit deleting my fucking repeatbackground script

--- Lesson 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Lesson 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,14 @@ public class PlayerController : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("GAME OVER!");
+        }
     }
 }
