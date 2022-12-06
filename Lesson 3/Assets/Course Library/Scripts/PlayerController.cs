2920720e8a893233585fa99eeabd43ced0f068ab
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2022-12-06 09:13:38 -0800

    added sound

--- Lesson 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Lesson 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+     private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         playerAnim = GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
         
     }
 
@@ -28,7 +32,8 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
-            dirtParticle.Stop();}
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);}
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -45,6 +50,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
