f45b685361b5eee72e4b3027e0ffec661e5750a0
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2023-01-18 09:13:36 -0800

    soo

--- unit 4/Assets/Course Library/Scripts/PlayerController.cs
+++ unit 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,17 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
 
     }
 }
