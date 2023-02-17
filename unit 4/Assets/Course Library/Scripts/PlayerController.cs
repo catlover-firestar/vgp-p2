287ee573f3079ea17615a413f53fe188493f760c
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2023-02-17 09:13:10 -0800

    Update PlayerController.cs

--- unit 4/Assets/Course Library/Scripts/PlayerController.cs
+++ unit 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public bool hasPowerup;
+    private float powerupStrength = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +36,10 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
            Debug.Log("collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
         }
     }
