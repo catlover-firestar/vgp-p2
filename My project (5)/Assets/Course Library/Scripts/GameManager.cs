fe271e8779f5f78a4a4fbc8fa60d8e677709f8b4
catlover-firestar <113128850+catlover-firestar@users.noreply.github.com>
2023-03-27 09:05:40 -0700

    hldf

--- My project (5)/Assets/Course Library/Scripts/GameManager.cs
+++ My project (5)/Assets/Course Library/Scripts/GameManager.cs
@@ -71,7 +71,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            ChangePaused()
+            ChangePaused();
         }
     }
     public void UpdateScore(int scoreToAdd)
